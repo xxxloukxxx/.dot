7e07771d24f60cbd2b7aca6878387bff25e755f2
TODARO Cédric <58140568+xxxloukxxx@users.noreply.github.com>
2023-08-09 08:54:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo '### Update system'
 sudo apt-get -qq update
 sudo apt-get -y -qq full-upgrade
 sudo apt-get -y -qq install sudo build-essential software-properties-common apt-transport-https aptitude curl psmisc locales-all micro vim p7zip-full unzip git openssh-client openssh-server suckless-tools stterm fish tmux xterm fonts-hack fonts-hack-otf fonts-hack-ttf fonts-hack-web fonts-font-awesome x11-utils libreadline-dev libx11-dev libxinerama-dev libxft-dev i3 i3blocks rsstail greetd xorg numlockx xrdp fzf fd-find bat nnn xdg-utils dex tree ripgrep feh btop adwaita-icon-theme > /dev/null
-sudo apt-get -y autoremove
+sudo apt-get -y autoremove > /dev/null
 
 ###################
 
